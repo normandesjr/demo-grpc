cfa258844c441eeb80d024354fca7fd3b6b9b077
Normandes Jose Moreira Junior <normandes.junior@zup.com.br>
2019-11-06 14:47:04 -0300

    First client/server unary gRPC

---
+++ src/main/java/com/hibicode/grpc/greeting/client/GreetingClient.java
@@ -0,0 +1,42 @@
+package com.hibicode.grpc.greeting.client;
+
+import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class GreetingClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello I'm a gRPC client");
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext() // allow work without SSL
+                .build();
+
+        System.out.println("Creating stub");
+//        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+//        DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
+
+        // blocking - synchronous
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("Normandes")
+                .setLastName("Junior")
+                .build();
+
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        GreetResponse greetResponse = greetClient.greet(greetRequest);
+        System.out.println(greetResponse.getResult());
+
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+    }
+
+}
